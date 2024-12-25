d1a7a97ade5400ef59339a2951c66378e2d4a3a8
Ingemar A. Poutsma <ingemar@live.no>
2024-12-25 23:43:53 +0100

    this is not good enough

--- day_2/main.c
+++ day_2/main.c
@@ -19,187 +19,188 @@ typedef unsigned __int64 u64;
 #define SWAP(a,b) if(&a != &b) {a^=b; b^=a; a^=b;}
 #define PARSE_INT(arr,i,b) b = 0; while((u8)(arr[i] - ('0')) < 10) { b = b*10 + (u8)(arr[i] - '0'); i++; }
 
+int 
+sign(int a)
+{
+	if(a < 0) return 1;
+	else return 0;
+}
+
+
+void
+printArr(int* arr, int length)
+{
+	for(int i = 0; i < length; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+
+int
+isUnsafe(int prev, int cur, u8 shouldDecr)
+{
+	int expected = prev + 1;
+	if(shouldDecr) expected = prev - 3;
+
+	if(cur < expected)			return 1;
+	else if(cur > expected + 2) return 2;
+	else						return 0;
+}
+
+
 void
 part1(u8* input, int inputSize)
 {
-	u64 total = 0;
-	int lastNum = 0;
-	int curNum = 0;
-	int decr = 0;
+	int total = 0;
 
+	int prev = 0;
+	int cur = 0;
+	int shouldDecr = 0;
 	int i = 0;
 	while(i < inputSize)
 	{
-		int flag = 0;
-		int lastNum = 0;
-		int curNum = 0;
-		PARSE_INT(input, i, lastNum);
-		if(input[i] == '\n') 
+		PARSE_INT(input,i,prev);
+		i++;
+		PARSE_INT(input,i,cur);
+		i++;
+		shouldDecr = sign(cur-prev);
+		
+		if(isUnsafe(prev, cur, shouldDecr))
 		{
-			total++;
+			while(input[i] != '\n') i++;
 			i++;
 			continue;
 		}
-		int t = i+1;
-		PARSE_INT(input, t, curNum);
-		decr = ((unsigned int)(curNum-lastNum))>>31;
+
 
 		while(input[i] != '\n')
 		{
-			i++;
-			curNum = 0;
-			PARSE_INT(input, i, curNum);
+			prev = cur;
+			PARSE_INT(input, i, cur);
 			
-			if(curNum == lastNum
-			|| abs(curNum-lastNum) > 3
-			|| (((unsigned int)(curNum-lastNum))>>31) != decr)
+			if(isUnsafe(prev, cur, shouldDecr))
 			{
-				while(input[i] != '\n') i++;
-				flag = 1;
+				while(input[i] != '\r') i++;
 				break;
 			}
-
-			lastNum = curNum;
+			i++;
 		}
 
-		if(!flag) total++;
+		if(input[i] == '\r') i++;
+		else total++;
+
 		i++;
 	}
 
-	printf("Part 1: %llu\n", total);
+	printf("part 1: %d\n", total);
 }
 
-int unsafe(int a, int b, unsigned int shouldDecr) { return (a == b || abs(a-b) > 3 || (((unsigned int)(b-a))>>31) != shouldDecr); }
+
+int
+isUnsafe2(int a, u8 shouldDecr)
+{
+	if(a == 0 || abs(a) > 3 || sign(a) != shouldDecr) return 1;
+	else											  return 0;
+}
+
+
+u8
+helper(int* der, u8 start, u8 end, u8 shouldDecr, u8 hasRemoved)
+{
+	for(int j = start; j < end; j++)
+	{
+		printf("	%d\n", der[j]);
+		if(isUnsafe2(der[j], shouldDecr))
+		{
+			if(hasRemoved)		return 0;
+			else if(j+1 == end) return 1;
+			else
+			{
+				printf("		%d\n", der[j-1]+der[j]);
+				printf("		%d\n", der[j]+der[j+1]);
+				if(!isUnsafe2(der[j-1] + der[j], shouldDecr)
+				|| !isUnsafe2(der[j] + der[j+1], shouldDecr))
+				{
+					u8 a = helper(der, j+1, end, shouldDecr, 1);
+					der[j+1] += der[j];
+					u8 b = helper(der, j+1, end, shouldDecr, 1);
+
+					if(a || b) return 1;
+				}
+				else return 0;
+			}
+		}
+	}
+
+	return 1;
+}
+
 
 void
 part2(u8* input, int inputSize)
 {
 	int total = 0;
-	int window[] = {0,0,0,0};
-	int shouldDecr = 0;
-
+	
 	int i = 0;
 	while(i < inputSize)
 	{
-		int j = 0;
-		int hasRemoved = 0;
-		int flag = 0;
-		int shouldDecr = 0;
+		int report[] = {0,0,0,0,0,0,0,0,0,0};
+		u8 repSize = 0;
 
-		PARSE_INT(input, i, window[0]);
-		i++;
-		j++;
-		PARSE_INT(input, i, window[1]);
-		int t = i + 1;
-		j++;
-		PARSE_INT(input, t, window[2]);
-		t++;
-		j++;
-		PARSE_INT(input, t, window[3]);
-		
-		shouldDecr = ((unsigned int)(window[1]-window[0]))>>31;
-		
-		int wind[] = {unsafe(window[0], window[1], (((unsigned int)(window[1]-window[0]))>>31)), //normal 0 and 1
-					  unsafe(window[1], window[2], (((unsigned int)(window[1]-window[0]))>>31)), //normal 1 and 2
-					  unsafe(window[2], window[3], (((unsigned int)(window[1]-window[0]))>>31)), //normal 2 and 3
-					  unsafe(window[1], window[2], (((unsigned int)(window[2]-window[1]))>>31)), //if 0 removed
-					  unsafe(window[0], window[2], (((unsigned int)(window[2]-window[0]))>>31)), //if 1 removed
-					  unsafe(window[2], window[3], (((unsigned int)(window[2]-window[0]))>>31)), //if 1 removed
-					  unsafe(window[2], window[3], (((unsigned int)(window[2]-window[1]))>>31))}; //if 0 removed
-		for(int k = 0; k < 7; k++)
+		int a = 0;
+		while(input[i] != '\n')
 		{
-			if(wind[k]) printf("%d\n", k);
-		}
-
-		if(wind[0])
-		{
-			if(!wind[4] && !wind[5])
-			{
-				shouldDecr = (((unsigned int)(window[2]-window[0]))>>31);
-				j--;
-				hasRemoved = 1;
-			}
-			else if(!wind[3] && !wind[6])
-			{
-				shouldDecr = (((unsigned int)(window[2]-window[1]))>>31);
-				hasRemoved = 1;
-			}
-			else 
-			{
-				printf("	unsafe1\n");
-				while(input[i] != '\n') i++;
-				flag = 1;
-			}
-		}
-		else if(wind[1])
-		{
-			if(!wind[4] && !wind[5])
-			{
-				j--;
-				shouldDecr = (((unsigned int)(window[2]-window[0]))>>31);
-				hasRemoved = 1;
-			}
-			else 
-			{
-				printf("	unsafe2\n");
-				while(input[i] != '\n') i++;
-				flag = 1;
-			}
-		}
-
-		j -= 2;
-		while(input[i] != '\n' && i < inputSize)
-		{
+			PARSE_INT(input, i, report[repSize]);
 			i++;
-			j++;
-
-			PARSE_INT(input, i, window[j%4]);
-			
-
-			if(unsafe(window[(j-1)%4], window[j%4], shouldDecr))
+			repSize++;
+		}
+		
+		u8 faults = 0;
+		for(int j = 1; j < repSize; j++) 
+		{
+			if(isUnsafe(report[j-1], report[j], sign(report[1]-report[0]))) faults++;
+		}
+		//printf("\n");
+		
+		u8 safe = 0;
+		if(faults == 0) 
+		{
+			safe = 1;
+			total++;
+		}
+		else
+		{
+			for(int k = 0; k < repSize; k++)
 			{
-				printf("%d %d\n", window[(j-1)%4], window[j%4]);
-				if(hasRemoved)
-				{
-					printf("	unsafe3\n");
-					while(input[i] != '\n' && i < inputSize) i++;
-					flag = 1;
-					break;
-				}
+				int t[] = {0,0,0,0,0,0,0,0,0,0};
+				for(int j = 0; j < k; j++) t[j] = report[j];
+				for(int j = k+1; j < repSize; j++) t[j-1] = report[j];
+				u8 shouldDecr = sign(t[1]-t[0]);
 
-				int next = 0;
-				t = i+1;
-				if(input[i] != '\n')
+				u8 f = 0;
+				for(int j = 1; j < repSize-1; j++)
 				{
-					PARSE_INT(input, t, next);
+					if(isUnsafe(t[j-1], t[j], shouldDecr)) 
+					{
+						//printf("	%d %d %d\n", t[j-1], t[j], shouldDecr);
+						f++;
+						break;
+					}
 				}
-				else break;	
-
-
-				
-				if(j>1 && !unsafe(window[(j-2)%4], window[j%4], shouldDecr) && !unsafe(window[j%4], next, shouldDecr))
+				//printf("%d  ", f);
+				if(f == 0) 
 				{
-					window[(j-1)%4] = window[j%4];
-					j--;
-					hasRemoved = 1;
-				}
-				else if(!unsafe(window[(j-1)%4], next, shouldDecr))
-				{
-					j--;
-					hasRemoved = 1;
-				}
-				else
-				{
-					printf("	unsafe4\n");
-					while(input[i] != '\n' && i < inputSize) i++;
-					flag = 1;
+					safe = 1;
+					total++;
 					break;
 				}
 			}
 		}
-		
-		if(!flag) total++;
-		//printf("\n");	
+		printf("%d ", safe);
+		printArr(report, repSize);
+
 		i++;
 	}
 
@@ -211,10 +212,7 @@ int
 main(int argc, u8** argv)
 {
 	INPUT;
-	for(int i = 0; i < 30; i++) printf("%d ", input[i]);
-	printf("\n");
 	part1(input, inputSize);
 	part2(input, inputSize);
-
 	return 0;
 }
